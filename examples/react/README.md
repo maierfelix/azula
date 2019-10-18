c7cf6151be0e8f610e601b79f54698ba6b35ef10
maierfelix <xilefmai@gmail.com>
2019-10-18 23:32:22 +0200

    Add React.js example

---
+++ examples/react/README.md
@@ -0,0 +1,7 @@
+# React
+
+This example shows the basic usage of *azula* together with React.js.
+
+### Preview:
+
+<img src="https://i.imgur.com/gUD8rfO.png"/>
---
+++ examples/react/index.mjs
@@ -0,0 +1,30 @@
+import fs from "fs";
+import azula from "azula";
+
+// create a window
+let window = new azula.Window({
+  width: 512,
+  height: 512,
+  title: "azula"
+});
+
+// forwards GUI console calls to node CLI
+window.onconsolemessage = e => {
+  let message = e.message;
+  let loc = `at ${e.source ? e.source + ":" : ""}${e.location.line}:${e.location.column}`;
+  e.callee.apply(console, [e.message, loc]);
+};
+
+// load the basic HTML file
+window.loadHTML(fs.readFileSync("./index.html", "utf8"));
+
+// execute the update loop
+// this loop runs as long as our window is active
+(function updateLoop() {
+  // abort, if the window was closed
+  if (window.shouldClose()) return;
+  // poll events (make the window interactive)
+  window.update();
+  // re-run this loop as soon as possible
+  setImmediate(updateLoop);
+})();
