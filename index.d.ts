c51e072773fe37210b8787518605b8f34f8f7867
piousdeer <31318219+piousdeer@users.noreply.github.com>
2019-10-19 00:01:03 +0700

    Add TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,66 @@
+export type MouseEventType =
+  | "onmousedown"
+  | "onmouseup"
+  | "onmousemove"
+
+export type KeyEventType =
+  | "onkeydown"
+  | "onkeyup"
+
+export type ScrollEventType =
+  | "onmousewheel"
+
+export interface WindowOptions {
+  width?: number;
+  height?: number;
+  title?: string;
+  useOffscreenRendering?: boolean;
+}
+
+export interface ResizeEvent {
+  width: number;
+  height: number;
+}
+
+export interface CursorChangeEvent {
+  name: string;
+}
+
+export interface ConsoleMessageEvent {
+  level: string;
+  callee: Function;
+  message: string;
+  source: string;
+  location: {
+    line: number;
+    column: number;
+  }
+}
+
+export class Window {
+  title: number;
+  width: number;
+  height: number;
+
+  constructor (options: WindowOptions);
+
+  update (): void;
+  flush (): void;
+  shouldClose (): boolean;
+
+  loadHTML (html: string): void;
+  loadFile (path: string): void;
+
+  onresize: (event: ResizeEvent) => any;
+  oncursorchange: (event: CursorChangeEvent) => any;
+  onconsolemessage: (event: ConsoleMessageEvent) => any;
+
+  dispatchMouseEvent (type: MouseEventType, x: number, y: number, button: number): void;
+  dispatchKeyEvent (type: KeyEventType, keyCode: number): void;
+  dispatchScrollEvent (type: ScrollEventType, deltaX: number, deltaY: number): void;
+
+  dispatchBinaryBuffer (buffer: ArrayBuffer, args?: object): void;
+  onbinarymessage: (buffer: ArrayBuffer, args?: object) => any;
+
+  getSharedHandleD3D11 (): BigInt;
+}
